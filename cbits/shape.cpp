9f8cc6ae9d60d0567934efb12da1af79ab72d340
Dmitrij Naumov <dmitrij@naumov.de>
2012-02-26 16:40:29 +0100

    Naive off file reader.

--- cbits/shape.cpp
+++ cbits/shape.cpp
@@ -1,4 +1,10 @@
 
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <utility>
+#include <vector>
+
 #include "shape.h"
 
 #include "btBulletDynamicsCommon.h"
@@ -48,4 +54,65 @@ void plSetScaling(plCollisionShapeHandle cshape, plVector3 cscaling)
 	shape->setLocalScaling(scaling);	
 }
 
+// Resulting vectors are always _triples_ of doubles (-> points) or integers (->
+// triangles).
+std::pair<std::vector<double>, std::vector<int> >
+readOffFile(const std::string filename)
+{
+    std::vector<double> points;
+    std::vector<int> triangles;
+
+    std::ifstream file(filename.c_str());
+    bool isBinary = false;
+
+    std::string tmpString;
+    // Read header.
+    std::getline(file, tmpString);
+    if (tmpString != std::string("OFF") && tmpString != std::string("OFF BINARY"))
+        // Error. Return empty points and triangles.
+        return std::make_pair(points, triangles);
+    if (tmpString == std::string("OFF BINARY")) {
+        // Reopen in binary mode and skip header.
+        isBinary = true;
+        const size_t pos = file.tellg();
+        file.close();
+        file.open(filename.c_str(), std::ios::in | std::ios::binary);
+        file.seekg(pos);
+    }
+
+    // Dimensions.
+    size_t nPoints, nTriangles, nEdges;
+    file >> nPoints >> nTriangles >> nEdges;
+
+    points.reserve(3*nPoints);
+    for (size_t i = 0; i < 3*nPoints; i++) {
+        double v;
+        file >> v;
+        points.push_back(v);
+    }
+
+    triangles.reserve(3*nTriangles);
+    for (size_t i = 0; i < nTriangles; i++) {
+        int v;
+        file >> v;
+        // Accept only triangles.
+        if (v != 3) {
+            triangles.clear();
+            return std::make_pair(points, triangles);
+        }
+        for (int j = 0; j < 3; j++) {
+            file >> v;
+            triangles.push_back(v);
+            if (isBinary) {
+                // Skip color.
+                file >> v;
+                double c;
+                for (int k = 0; k < v; k++)
+                    file >> c;
+            }
+        }
+    }
+
+    return std::make_pair(points, triangles);
+}
 
